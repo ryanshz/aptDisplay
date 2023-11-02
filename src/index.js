87f4f993b4301cc798e960933a94189a79658941
legootter <77215575+legootter@users.noreply.github.com>
2023-11-02 00:37:29 -0400

    Modified __dirname to change depending on the os and fixed default.css route

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import child_process from "child_process"
 import express from "express";
 const app=express()
 const port=8081;
-const __dirname=import.meta.url.split("///")[1].split("index")[0];
+let __dirname="/"+import.meta.url.split("///")[1].split("index")[0];
+if(os.platform()=="win32") __dirname=import.meta.url.split("///")[1].split("index")[0];
 
 
 app.all("/",(req,res)=>{
-    res.sendFile(__dirname+"index.html")
+    res.sendFile("/"+__dirname+"index.html")
     console.log(__dirname+"index.html")
 })
 
 
-app.get("/index.css",(req,res)=>{
-    res.sendFile(__dirname+"index.css")
+app.get("/default.css",(req,res)=>{
+    res.sendFile(__dirname+"default.css")
+    console.log(__dirname+"default.css")
 })
 
 app.listen(port,()=>{
+    console.log(`Server started at: http://localhost:${port}`);
+    console.log(`To stop server, use CTRL + C`);
     if(os.platform()=="linux"&&os.arch()=="arm"){
         child_process.exec(`DISPLAY=:0 chromium-browser --kiosk http://127.0.0.1:${port}`,function(error, stdout, stderr) {
             console.dir(stdout);
